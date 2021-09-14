0bbad147da9efc7b0b72d86ad8d43f412000438b
GuangjiHuang <GuangJiHuang@qq.com>
2021-09-14 22:33:42 +0800

    update->2021-09-14 Tue 22:33:41

---
+++ c++-test/main_101.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "the size of the short int is the: " << sizeof(short int) << endl;
+    cout << "the size of the int is the: " << sizeof(int) << endl;
+    cout << "the size of the long is the: " << sizeof(long) << endl;
+    cout << "the size of the long long int is the: " << sizeof(long long) << endl;
+
+
+    return 0;
+}
